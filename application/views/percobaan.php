d4ff8b3b8cf8e0e3d45bee0967a32bf2951c251a
Ubaidillah Ahmad <ubaidillaha657@gmail.com>
2023-08-12 18:36:28 +0700

    first commit

---
+++ application/views/percobaan.php
@@ -0,0 +1,5 @@
+<h1>Ini Halaman Percobaan</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt eum recusandae quo culpa? Laudantium enim ipsam perspiciatis necessitatibus aut quasi, vero accusamus, iusto eos minima deserunt ab? Assumenda, dolorum non!</p>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate unde explicabo nostrum ullam dolor natus quis ea, nisi rerum modi inventore, doloribus deleniti. Neque praesentium libero eaque accusamus eum nostrum.</p>
