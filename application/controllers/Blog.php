9817f0c4f37207c28de930887063ae235aa14297
Ubaidillah Ahmad <ubaidillaha657@gmail.com>
2023-08-14 10:34:07 +0700

    penghapusan comment dan helper

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -4,27 +4,11 @@ Class Blog extends CI_Controller
     public function index()
     {
         $this->load->database();
-        $this->load->helper('url');
 
         $query=$this->db->get("blog");
         $data['blogs']=$query->result_array();
         
         $this->load->view('blog',$data);
-
-        // $data['blogs']=[
-        //     ['title'=>'Artikel Pertama',
-        //     'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and     
-        //      typesetting industry.</p>'],
-        //     ['title'=>'Artikel Kedua',
-        //     'content'=>'<p>Lorem Ipsum has been the industrys standard dummy 
-        //      text ever since the 1500s, when an unknown printer took a galley of type 
-        //      and scrambled it to make a type specimen book.</p>'],
-        //     ['title'=>'Artikel Ketiga',
-        //     'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and     
-        //      typesetting industry. Lorem Ipsum has been the industrys standard dummy 
-        //      text ever since the 1500s.</p>']
-        //     ];
-        // $this->load->view('blog',$data);
 
     }
 }
--- application/views/blog.php
+++ application/views/blog.php
@@ -5,15 +5,12 @@
 </head>
 <body>
     <h1>Artikel Terbaru</h1>
-    <?php foreach($blogs as $key=>$blog):?>
-    <div class="blog">
-        <h2>
-            <a href="<?php echo site_url('blog/detail/ '.$blog['url']);?>">
-                <?php echo $blog['title'];?>
-            </a>
-        </h2>
-        <?php echo $blog['content'];
-        endforeach;
-    ?>  
+    <?php foreach ($blogs as $key => $blog): ?>
+        <div class="blog">
+        <h2><?php echo $blog['title'];?></h2>
+        <?php echo $blog['content'];?>
+        </div>
+        
+       <?php endforeach; ?>  
 </body>
 </html>
